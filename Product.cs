c28436ecc4348f027fe3aa1517991859c173e272
Thor7578 <56076721+Thor7578@users.noreply.github.com>
2021-06-09 16:03:08 +0200

    Add project files.

---
+++ Product.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExamTestRun
+{
+    class Product
+    {
+        // ID
+        private int _id;
+        private String _name;
+        private int _amount;
+
+        //public Product(int _id, String _name, int _amount)
+        //{
+        //    this._id = _id;
+        //    this._name = _name;
+        //    this._amount = _amount;
+
+        //}
+        public int ProductId
+        {
+            get
+            {
+                return _id;
+            }
+            set
+            {
+                _id = value;
+            }
+
+        }
+        // Navn
+        public String ProductName
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+            }
+        }
+        // Antal
+        public int ProductAmount
+        {
+            get
+            {
+                return _amount;
+            }
+            set
+            {
+                _amount = value;
+            }
+        }
+        public override string ToString()
+        {
+            return $"et ekstra produkt: {ProductId}    {ProductName}   {ProductAmount}";
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExamTestRun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int[] stingRay = { 1, 2, 3, 4, 5, 6 };
+                String[] weRay = { "Hej. ", "Troels ", "skal ", "lære ", "mig ", "at ", "tælle" };
+                //Console.WriteLine(stingRay[10]);
+                var viffelVaffelObviously = new List<Product>();
+
+
+                for (int i = 0; i < stingRay.Length; i++)
+                  {
+                     //Console.WriteLine(stingRay[i]);
+                  }
+
+                foreach (var j in stingRay)
+                   {
+                       Console.WriteLine(j);
+
+                   }
+
+                // String array printet
+                //foreach (var k in weRay)
+                //{
+                //    Console.Write(k);
+                //}
+
+                int num = 0;
+                while (num < weRay.Length)
+                    {
+                        Console.Write(weRay[num]);
+                        num++;
+                    }
+                   Console.WriteLine();
+                Product product = new Product();
+                Product product2 = new Product();
+                Product product3 = new Product();
+
+                product.ProductId = 1;
+                product.ProductName = "Mogens";
+                product.ProductAmount = 1337;
+
+                product3.ProductId = 2;
+                product3.ProductName = "Lars";
+                product3.ProductAmount = 420;
+
+                product2.ProductId = 3;
+                product2.ProductName = "HR";
+                product2.ProductAmount = 42206361;
+
+                viffelVaffelObviously.Add(product);
+                viffelVaffelObviously.Add(product2);
+                viffelVaffelObviously.Add(product3);
+
+                foreach (var i in viffelVaffelObviously)
+                {
+                    Console.WriteLine(i);
+                }
+
+                //viffelVaffelObviously = Type.GetType("System.Type").GetProperties();
+                //for (int i = 0; i < viffelVaffelObviously.Length; i++)
+                //{
+
+                //}
+
+
+
+            //    String printList = viffelVaffelObviously.ToString();
+            //    Console.WriteLine(printList);
+            }
+            catch (Exception ex)
+                {
+
+                    Console.WriteLine("Der skete en fejl. ");
+                }
+            
+
+            
+        }
+    }
+}
